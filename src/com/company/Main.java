91cd0d5216f191dccb24432a67d66b5bfd0d5c73
wehelie <mawehelie@gmail.com>
2017-10-05 01:34:11 -0700

    add comments, rearranged some of the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,24 @@
+/*=============================================================================
+ |      Assignment:  Program #[1]:  [Program 1]
+ |      Author:  [Leyth Adan]
+ |      Grader:  [jstager]
+ |
+ |      Course:  [ad300]
+ |      Instructor:  John C. Stager
+ |      Due Date:  [9 October 2017]
+ |
+ |      Description:   Write a Java program that accepts 10 integers through the
+ |                     console and places them in an array.  Your input should only
+ |                     accept 10 numbers and only integers.  The program should
+ |                     calculate and display the average of the ten numbers on the console
+ |
+ |      Language:  Java
+ *===========================================================================*/
+
 package com.company;
 // * is a wildcard which means that you're importing all the classes in the particular package
-import java.util.*; // Import all the classes in the util package.
+
+import java.util.Scanner;
 
 /**
  * The Main class implements an application that
@@ -26,10 +44,10 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             array[i] = scanf.nextInt();
         }
-        for (int integ: array) {
+        for (int integ : array) {
             sum += integ;
         }
         // console the results
-        System.out.println(String.format("The average comes out to be: %d", sum/array.length));
+        System.out.println(String.format("The average comes out to be: %d", sum / array.length));
     }
 }
